117a3846b94495c8d788c4b339fdec2707c5e4ca
buzzhaha <boming.fan@gmail.com>
2017-12-20 13:58:26 -0800

    env-test

--- key.js
+++ key.js
@@ -1,10 +1,11 @@
-var Twitter = require("twitter");
+console.log('this is loaded');
 
-var twitterKeys = new Twitter({
-    consumer_key: '	0Mx6i5M5HhLSd8QiR4ArHr1CI',
-    consumer_secret: 'OCVvTgmjbLUVsfAz1GLazTdkRWlDoxeX6EcfsRBhwNYxEt6qxK',
-    access_token_key: '247085509-ISaTdwpRy7b5c5wmVMU21QZ1JE6vxnfYUziWzaea',
-    access_token_secret: 'ahRhl7fiBbUmREYQN0O7sH1YDZa2zH9zryBCAlGe3Ft51',
-  });
-  
-module.exports = twitterKeys;
+var Twitter = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  };
+
+
+module.exports = Twitter;
--- liri.js
+++ liri.js
@@ -1,14 +1,12 @@
-// var client = require("./key.js");
+
+
 
 var Twitter = require("twitter");
+require("dotenv").config();
 
-var client = new Twitter({
-    consumer_key: '0Mx6i5M5HhLSd8QiR4ArHr1CI',
-    consumer_secret: 'OCVvTgmjbLUVsfAz1GLazTdkRWlDoxeX6EcfsRBhwNYxEt6qxK',
-    access_token_key: '247085509-ISaTdwpRy7b5c5wmVMU21QZ1JE6vxnfYUziWzaea',
-    access_token_secret: 'ahRhl7fiBbUmREYQN0O7sH1YDZa2zH9zryBCAlGe3Ft51'
-  });
+var twitterKeys= require("./key.js");
 
+var client = new Twitter(twitterKeys);
 
 
 var params = {screen_name: 'buyitunesgift'};
@@ -21,3 +19,5 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
     }
   }
 });
+
+
